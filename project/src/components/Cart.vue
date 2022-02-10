5e5250b18369ae68476e1b8544b9c55330759d96
EricWeng23 <ericw39@nycstudents.net>
2022-02-10 13:58:26 -0500

    dwada

--- project/src/components/Cart.vue
+++ project/src/components/Cart.vue
@@ -5,21 +5,22 @@
         <h2 v-if="this.carts.length === 0">Please Don't Be Cheap and Spent More Money</h2>
         <h2 v-if="this.carts.length > 0">Total: ${{total}}</h2>
     <div class="cart_items">
-        <div v-for="cart in carts" :key="cart.name" class="cart_item">
-             <h2>{{cart.name}}</h2>
-        <img :src="cart.image" :alt="cart.name" class="cart_img"/>
-        <h3>${{cart.price}}</h3>
-        <button v-on:click="removeItem(cart)" class="remove_item">Remove from Cart</button>
-        </div>
+        <CartItem v-for="cart in carts" 
+        :key="cart.name" 
+        :name="cart.name"
+        :image="cart.img"
+        :price="cart.price"
+        />
     </div>
     
 </div>
 </template>
 
 <script>
+import CartItem from '../components/CartItem.vue'
 export default {
     name: "Cart",
-    components: {},
+    components: {CartItem},
     methods: {
         clear() {
       this.carts.splice(0);
@@ -40,35 +41,6 @@ export default {
 </script>
 
 <style scoped>
-.cart_items {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
-}
-
-.cart_item {
-  margin-top: 2rem;
-  border: 1px solid lightgray;
-  border-radius: 10px;
-  box-shadow: 10px 5px 5px rgba(0, 0, 0, 0.555);
-  width: 30%;
-  height: 500px;
-  transition-duration: all ease 2s;
-}
-
-.cart_item:hover {
-  transform: scale(1.05);
-}
-img {
-  width: 30%;
-  height: 50%;
-}
-.remove_item{
-  width: 40%;
-  height: 10%;
-  background-color: aquamarine;
-  font-size: 1rem;
-}
 
 </style>
 
--- project/src/components/CartItem.vue
+++ project/src/components/CartItem.vue
@@ -1,10 +1,9 @@
 <template>
     <div class="cart_items">
-        <div class="cart_item">
             <h2 class="cart_name">{{name}}</h2>
+            <img src="img" alt="">
             <h2 class="cart_price">${{price}}</h2>
-            <img :src="image" alt="">
-        </div>
+            <button v-on:click="removeItem(cart)" class="remove_item">Remove from Cart</button>
     </div>
 </template>
 
@@ -15,6 +14,49 @@ export default {
         name: String,
         image: String,
         price: Number,
+    },
+     methods: {
+       removeItem(cart) {
+      this.$store.commit("remove", cart);
+    },  
+    },
+    computed: {
+        carts() {
+            return this.$store.state.cart;
+        },
     }
 }
 </script>
+
+<style scoped>
+.cart_items {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+}
+
+.cart_items {
+  margin-top: 2rem;
+  border: 1px solid lightgray;
+  border-radius: 10px;
+  box-shadow: 10px 5px 5px rgba(0, 0, 0, 0.555);
+  width: 30%;
+  height: 500px;
+  transition-duration: all ease 2s;
+}
+
+.cart_items:hover {
+  transform: scale(1.05);
+}
+img {
+  width: 30%;
+  height: 50%;
+}
+.remove_item{
+  width: 40%;
+  height: 10%;
+  background-color: aquamarine;
+  font-size: 1rem;
+}
+
+</style>
